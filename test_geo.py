d589ef999668b872be8159acf4e3d2d3a1a20fda
Jonathan Pereira <jp943@cam.ac.uk>
2022-02-10 13:07:53 +0000

    Merging all geo tests into one file

--- test_geo.py
+++ test_geo.py
@@ -1,5 +1,7 @@
 #testing for geo functions
 
+from floodsystem.geo import rivers_by_station_number
+from floodsystem.geo import stations_by_river
 from floodsystem.geo import rivers_with_station, stations_by_distance, stations_by_river, stations_within_radius
 from floodsystem.stationdata import build_station_list
 
@@ -16,3 +18,18 @@ def test_stations_within_radius():
     assert len(stations_within_radius(stations,p,1000 )) > 1
 
 #test_stations_within_radius()
+
+def test_rivers_by_station_number():
+    '''check if the fucntion has formed a list'''
+    stations = build_station_list() 
+    assert len(rivers_by_station_number(stations, 9)) > 1
+
+#test_rivers_by_station_number() 
+
+
+def test_stations_by_river():
+    '''check that stations by river has returned a non empty dictionary'''
+    stations = build_station_list()
+    assert len(stations_by_river(stations)) > 1
+
+#test_stations_by_river()
