7554cfb2272fc41ac664b21ef9380bd2a4d005ea
Ansh Sharma <sharmaansh176@gmail.com>
2022-02-10 12:48:01 +0000

    updated geo test file

--- test_geo.py
+++ test_geo.py
@@ -1,1 +1,14 @@
 #testing for geo functions
+
+from floodsystem.geo import haversine, rivers_with_station, stations_by_distance, stations_by_river, stations_within_radius
+
+
+haversine
+
+stations_by_distance
+
+stations_within_radius
+
+stations_by_river
+
+rivers_with_station
