48fbc75b62de544936defb39c7d25c7a2e48cb32
Ansh Sharma <sharmaansh176@gmail.com>
2022-02-10 13:05:37 +0000

    added unit tests for geo

--- test_geo.py
+++ test_geo.py
@@ -1,14 +1,18 @@
 #testing for geo functions
 
-from floodsystem.geo import haversine, rivers_with_station, stations_by_distance, stations_by_river, stations_within_radius
+from floodsystem.geo import rivers_with_station, stations_by_distance, stations_by_river, stations_within_radius
+from floodsystem.stationdata import build_station_list
 
+def test_stations_by_distance():
+    stations = build_station_list()
+    distances = stations_by_distance(stations, (0,0))
+    assert type(distances[0][1]) == float
 
-haversine
+#test_stations_by_distance()
 
-stations_by_distance
+def test_stations_within_radius():
+    stations = build_station_list()
+    p = (52.2053, 0.1218)
+    assert len(stations_within_radius(stations,p,1000 )) > 1
 
-stations_within_radius
-
-stations_by_river
-
-rivers_with_station
+#test_stations_within_radius()
