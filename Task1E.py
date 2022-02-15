3769fb25aa208dde947f68b9b84a90ae81ec046e
Ansh Sharma <sharmaansh176@gmail.com>
2022-02-15 19:28:59 +0000

    Fixed 1E

--- Task1E.py
+++ Task1E.py
@@ -5,7 +5,7 @@ from floodsystem.geo import rivers_by_station_number
 #runs for N=9 as required by the deliverables section
 def run():
     stations = build_station_list()
-    N = 10
+    N = 9
     print(rivers_by_station_number(stations, N))
 
 if __name__ == "__main__":
