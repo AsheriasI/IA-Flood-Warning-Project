931364bf0ad93c326240884096fcee131c8ccb92
Ansh Sharma <sharmaansh176@gmail.com>
2022-03-01 18:15:24 +0000

    testing tests

---
+++ test_flood.py
@@ -0,0 +1,11 @@
+from numpy import floor_divide
+from floodsystem.stationdata import build_station_list
+import floodsystem.flood as flood 
+
+def test_station_level_over_threshold():
+    stations = build_station_list()
+    flood.stations_level_over_threshold(stations, 0.8)
+
+def test_stations_highest_rel_level():
+    stations = build_station_list()
+    flood.stations_highest_rel_level(stations, 10)
